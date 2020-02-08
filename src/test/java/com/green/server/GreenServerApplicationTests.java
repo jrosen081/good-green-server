1f017458b2aafa82322f4aaebeae68cc9980165d
Andrew Johnson <anjohnson@veracode.com>
2020-02-08 00:29:37 -0500

    initial example commit

---
+++ src/test/java/com/green/server/GreenServerApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.com.green.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
